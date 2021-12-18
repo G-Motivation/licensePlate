8332dfb1ed1ed088ebe9028a6df1b0a956facb42
Willy <willy.du@ulictek.com>
2021-12-19 00:49:37 +0800

    Add OpenCV into the QT project.

--- main.cpp
+++ main.cpp
@@ -6,6 +6,9 @@
 
 #include <QQmlComponent>
 
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#define TRUN_ON_OPENCV 0
 
 int main(int argc, char *argv[])
 {
@@ -23,5 +26,13 @@ int main(int argc, char *argv[])
     QObject *item = mainPage->findChild<QObject *>("buttonTest");
     item->setProperty("text", "Text from C++");
 
+#if TRUN_ON_OPENCV
+    // read an image
+    cv::Mat image = cv::imread("C:\\Users\\willy\\Desktop\\9b060ac5f7e3434585b5c69000318898.jpeg", 1);
+    // create image window named "My Image"
+    cv::namedWindow("My Image");
+    // show the image on window
+    cv::imshow("My Image", image);
+#endif
     return app.exec();
 }
