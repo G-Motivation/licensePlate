ab55eba43e4b23cb231eda532aee8825d37a430b
Willy <qaz33338@gmail.com>
2022-05-07 21:53:24 +0800

    Update gtest

--- main.cpp
+++ main.cpp
@@ -11,9 +11,14 @@
 using namespace testing;
 
 #if TRUN_ON_TEST
-TEST(Factorial, Empty)
+bool check()
+{
+    return true;
+}
+TEST(Function, check)
 {
     EXPECT_EQ(1, 1);
+    EXPECT_TRUE(check());
 }
 #endif
 int main(int argc, char *argv[])
@@ -21,6 +26,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     #if TRUN_ON_TEST
     ::testing::InitGoogleTest(&argc, argv);
+    RUN_ALL_TESTS();
     #endif
     licensePlateDialog w;
     w.show();
@@ -31,8 +37,5 @@ int main(int argc, char *argv[])
     // show the image on window
     cv::imshow("My Image", image);
 #endif
-    #if TRUN_ON_TEST
-    RUN_ALL_TESTS();
-    #endif
     return a.exec();
 }
